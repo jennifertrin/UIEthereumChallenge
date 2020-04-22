61a1c720fdb0aceb3a03302d2a3f762d8011054d
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2020-04-22 18:56:17 -0400

    add NatSpec comments

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -1,66 +1,69 @@
+/// @title Contribution Smart Contract
+/// @author Jennifer Tran
+/// @notice A smart contract that allows users to donate ETH to and provides tokens in return
 pragma solidity ^0.6.0; 
 
-//imports
-import "./TimeToken.sol" as TimeToken; //TimeToken Contract
-import "@openzeppelin/contracts/math/SafeMath.sol" as SafeMath;  //SafeMath Library from OpenZeppelin
+///@notice imports
+import "./TimeToken.sol" as TimeToken; ///@dev TimeToken Contract
+import "@openzeppelin/contracts/math/SafeMath.sol" as SafeMath;  ///@dev SafeMath Library from OpenZeppelin
 
 contract Contribution is TimeToken {
 
-    using SafeMath for uint256; //use SafeMath library
+    using SafeMath for uint256; ///@dev use SafeMath library
 
     address owner;
 
-    //events
-    //event to track new donation
+    ///@notice events
+    ///@notice event to track new donation
     event NewDonation (
         address from,
         uint amount
     );
 
-    //event to track requests to view balance
+    ///@notice event to track requests to view balance
     event ViewBalance (
         uint balance,
         address balanceAddress
     );
 
-    //event to track when contract token has been rewarded to donor
+    ///@notice event to track when contract token has been rewarded to donor
     event returnToken (
         uint tokenAmount,
         address from
     );
 
-    //stores information of each donor
+    ///@notice stores information of each donor
     struct Donor {
         uint date;
         address donorAddress;
         uint amount;
     }
 
-    //associative array that stores address as a key and Donor as the value
+    ///@notice associative array that stores address as a key and Donor as the value
     mapping (address => Donor) public donors;
 
     constructor () public {
         owner = msg.sender;
     }
 
-    //fallback function to send Ether from address to this smart contract
+    ///@notice fallback function to send Ether from address to this smart contract
     function donate() external payable {
-        require(msg.value >= 0); //require donation to be more than 0 wei
+        require(msg.value >= 0); ///@dev require donation to be more than 0 wei
         balances[owner].add(msg.value);
-        emit NewDonation(owner, msg.value); //emit NewDonation event
+        emit NewDonation(owner, msg.value); ///@dev emit NewDonation event
     }
 
-    //function that sends ERC20 automatically to donor
+    ///@notice function that sends ERC20 automatically to donor
     function tokensend() payable public {
         TimeToken token token = TimeToken();
         token.transfer(msg.sender, 100);
-        emit returnToken(msg.value, msg.sender); //emit returnToken event
+        emit returnToken(msg.value, msg.sender); ///@dev emit returnToken event
     }
 
-    //function that returns the donation amount of the address
+    ///@notice function that returns the donation amount of the address
     function checkDonation(address _address) public view returns(uint) {
         totalDonation = donors[_address];
-        emit viewBalance(totalDonation, _address); //emit viewBalance event
+        emit viewBalance(totalDonation, _address); ///@dev emit viewBalance event
         return totalDonation;
     }
 }
--- contracts/TimeToken.sol
+++ contracts/TimeToken.sol
@@ -1,32 +1,32 @@
-/// @title A smart contract that extends ERC20Mintable contract from Open Zeppelin and allows token transfer only between specific start and end times
+/// @title TimeToken Smart Contract
 /// @author Jennifer Tran
-/// @notice You can use this contract for only the most basic simulation
-/// @dev All function calls are currently implemented without side effects
+/// @notice A smart contract that extends ERC20Mintable contract from Open Zeppelin and allows token transfer only between specific start and end times
 
 pragma solidity ^0.6.0; 
 
-//imports
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol" as ERC20Mintable; //ERC20Mintable from Open Zeppelin library
+///@dev imports
+///@notice ERC20Mintable from Open Zeppelin library
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol" as ERC20Mintable; 
 
 contract TimeToken is ERC20Mintable {
     uint256 starttime;
     uint256 endtime;
     
-    //function that allow token transfer only between a start and end times
+    ///@notice function that allow token transfer only between a start and end times
     function requiretime(uint256 _starttime, uint256 _endtime) public view {
         uint256 _currenttime = now; //current time
-        require(_currenttime >= _starttime); //require current time is after or equal to start time
-        require(_currenttime <= _endtime); //require current time is before or equal to end time
+        require(_currenttime >= _starttime); ///@dev require current time is after or equal to start time
+        require(_currenttime <= _endtime); ///@dev require current time is before or equal to end time
 
         _starttime = starttime;
         _endtime = endtime;
     }
     
-    //function that checks if the current time is between the start and end times
+    ///@notice function that checks if the current time is between the start and end times
     function checktime() public view returns(bool isCheck) {
-        bool isAfterStart = (now >= starttime); //checks that current time is after or equal to start time
-        bool isBeforeEnd = (now <= endtime); //checks that current time is before or equal to end time
-        bool isWithinTimeframe = isAfterStart && isBeforeEnd; //checks both conditions are met
+        bool isAfterStart = (now >= starttime); ///@dev checks that current time is after or equal to start time
+        bool isBeforeEnd = (now <= endtime); ///@dev checks that current time is before or equal to end time
+        bool isWithinTimeframe = isAfterStart && isBeforeEnd; ///@dev checks both conditions are met
         return isWithinTimeframe;
     }
 }
