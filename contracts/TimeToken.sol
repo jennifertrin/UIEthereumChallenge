db77177d6aeb4f50b5f2c8e45b2927a5752a968e
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2020-04-22 23:51:36 -0400

    Testing pragma solidity

--- contracts/TimeToken.sol
+++ contracts/TimeToken.sol
@@ -2,17 +2,17 @@
 /// @author Jennifer Tran
 /// @notice A smart contract that extends ERC20Mintable contract from Open Zeppelin and allows token transfer only between specific start and end times
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.0;
 
 ///@dev imports
 ///@notice ERC20Mintable from Open Zeppelin library
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol" as ERC20Mintable; 
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol" as ERC20Mintable;
 
 contract TimeToken is ERC20Mintable {
     uint256 private starttime;
     uint256 private endtime;
     
-    ///@notice function that allows token transfer only between a start and end times
+    ///@notice function that allow  token transfer only between a start and end times
     ///@param _starttime is the start time of the donation window
     ///@param _sendtime is the end time of the donation window
     ///@param address is the address that the token is minted to
@@ -21,7 +21,7 @@ contract TimeToken is ERC20Mintable {
     function requiretime(uint256 _starttime, uint256 _endtime, address _account, uint value) public view {
         uint256 _currenttime = now; ///@dev current time
         if (_currenttime >= _starttime && _currenttime <= _starttime) {
-            mint(_account, value); ///@dev using the mint function from ERCMintable Contract
+            mint(_account, value); ///@dev using the mint function from ERCMintable COntract
             _starttime = starttime;
             _endtime = endtime;
         }
