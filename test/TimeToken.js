f3afc57701fbd5d4a6ec82b57031269ef6dcf31b
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2020-04-22 21:08:12 -0400

    combined tests into one file, deleted previous tests for only TimeToken.sol

--- test/TimeToken.js
+++ test/TimeToken.js
@@ -1,31 +1,16 @@
 const TimeToken = artifacts.require('TimeToken'); //test for TimeToken contract
-//create variables to store current time
+const Contribution = artifacts.require('Contribution'); //test for Contribution contract
+
+//create variables used in the functions in the TimeToken and Contribution contracts
+
+//create variables for start and end times
 var today = new Date(); 
 var now = today.getTime(); //stores current time in milliseconds since epoch
+var _startTime = now - 2000;
+var _endTime = now + 4000;
 
-contract('TimeToken', () => {
-    //first test
-    it('Should return true if start time is before and end time is after the current time', async () => {
-        const contractInstance = await TimeToken.new();
-        var starttime = now - 200; 
-        var endtime = now + 200;
-        const result = await contractInstance.checktime();
-        assert(result == true); 
-    });
-    //second test
-    it('Should return false if start time is after the current time', async () => {
-        const contractInstance = await TimeToken.new();
-        var starttime = now + 200;
-        var endtime = now + 400;
-        const result = await contractInstance.checktime();
-        assert(result == false);
-    });
-    //third test
-    it('Should return false if end time is before the current time', async () => {
-        const contractInstance = await TimeToken.new();
-        var starttime = now - 600;
-        var endtime = now - 400;
-        const result = await contractInstance.checktime();
-        assert(result == false);
-    });
+var amount = 10; //donation amount
+var ethToTokenRate = 8; //ETH to token conversion rate
+
+contract('DonationTests', () => {
 })
