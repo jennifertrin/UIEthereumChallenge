3c0840cbf60470c56335dc0eec2b0191626abcac
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2020-04-22 20:55:19 -0400

    updated donate() function and included function for rate conversion

--- contracts/Contribution.sol
+++ contracts/Contribution.sol
@@ -42,25 +42,39 @@ contract Contribution is TimeToken {
     ///@dev associative array that stores address as a key and Donor as the value
     mapping (address => Donor) public donors;
 
-    ///@dev token contract reference to avoid hard coding
+    ///@dev token contract reference
     Token private contractReference;
+
+`   ///@dev stores rate to convert ETH to token value
+    uint ethToTokenRate;
 
     constructor (address _tokenReference) public {
         contractReference = Token(_tokenReference);
+        ethToTokenRate = _ethToTokenRate;
+
     }
 
-    ///@notice fallback function to send Ether from address to this smart contract
+    ///@notice fallback function to send Ether from address to this smart contract and then automatically provides token from smart contract to donor
     ///@param amount is the amount donated
     function donate(uint amount) external payable {
         require(amount >= 0); ///@dev require donation to be more than 0 wei
-        balances[msg.sender].add(amount);
+        balances[msg.sender].add(amount); 
         emit NewDonation(msg.sender, amount); ///@dev emit NewDonation event
-        TimeToken token token = TimeToken();
-        contractReference.transfer(msg.sender, amount);
-        emit returnToken(msg.value, msg.sender); ///@dev emit returnToken event
+        ///@dev start of the automatic send of the smart contract token to the donor 
+        uint tokenSend = convertETHtoToken(amount); ///@dev conversion from ETH to token using convertETHtoToken private function stored in contract
+        contractReference.transfer(msg.sender, tokenSend); ///@dev transfer of token to donor
+        emit returnToken(amount, msg.sender); ///@dev emit returnToken event
     }
 
-    ///@notice function that returns the donation amount of the address
+    ///@notice private view function that converts ETH to token rate using mul function in Safe Math
+    ///@param amount is the uint amount in ETH
+    ///@return returns the conversion from ETH to token, amount in the token
+    function convertETHtoToken(uint amount) private view returns(uint) {
+        conversion = ethToTokenRate.mul(amount); ///@dev use of mul function in Safe Math library
+        return conversion;
+    }
+
+    ///@notice public view function that returns the donation amount of the address
     ///@param address is the address that you would like to check
     ///@return returns the amount that the address has donated to this contract
     function checkDonation(address _address) public view returns(uint) {
